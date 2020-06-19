1376232a467bc734d7ec6299adb4c935e1900f57
Julian Nicholls <juliannicholls29@gmail.com>
2020-06-19 16:02:23 +0100

    Restore filters to Users and Posts queries

--- prisma/src/resolvers/Query.js
+++ prisma/src/resolvers/Query.js
@@ -2,10 +2,26 @@
 
 export default {
   users: async (_parent, args, { prisma }, info) => {
-    return prisma.query.users(null, info);
+    const pArgs = {};
+
+    if (args.query) {
+      pArgs.where = {
+        OR: [{ name_contains: args.query }, { email_contains: args.query }],
+      };
+    }
+
+    return prisma.query.users(pArgs, info);
   },
   posts: (_parent, args, { prisma }, info) => {
-    return prisma.query.posts(null, info);
+    const pArgs = {};
+
+    if (args.query) {
+      pArgs.where = {
+        OR: [{ title_contains: args.query }, { body_contains: args.query }],
+      };
+    }
+
+    return prisma.query.posts(pArgs, info);
   },
   comments: (_parent, _args, { prisma }, info) => {
     return prisma.query.comments(null, info);
