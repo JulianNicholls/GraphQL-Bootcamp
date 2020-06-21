08430f25fcfbd3e5f13d08494b627bb0720eede3
Julian Nicholls <juliannicholls29@gmail.com>
2020-06-21 21:17:34 +0100

    Update createUser and deleteUser to use prisma

--- prisma/src/resolvers/Mutation.js
+++ prisma/src/resolvers/Mutation.js
@@ -1,16 +1,12 @@
 import { v4 as uuid } from 'uuid';
 
 export default {
-  createUser: (_parent, { data }, { db }) => {
-    const dup = db.users.some(({ email }) => email === data.email);
+  createUser: async (_parent, { data }, { prisma }, info) => {
+    const dup = await prisma.exists.User({ email: data.email });
 
     if (dup) throw new Error('Email address is already in use.');
 
-    const newUser = { id: uuid(), ...data };
-
-    db.users.push(newUser);
-
-    return newUser;
+    return prisma.mutation.createUser({ data }, info);
   },
   createPost: (_parent, { data }, { db, pubsub }) => {
     const userFound = db.users.some(({ id }) => id === data.author);
@@ -135,24 +131,12 @@ export default {
 
     return comment;
   },
-  deleteUser: (_parent, args, { db }) => {
-    const userIndex = db.users.findIndex(({ id }) => id === args.id);
+  deleteUser: async (_parent, { id }, { prisma }, info) => {
+    const userExists = await prisma.exists.User({ id });
 
-    if (userIndex === -1) throw new Error('User is not recognised');
+    if (!userExists) throw new Error('User is not recognised');
 
-    const [user] = db.users.splice(userIndex, 1);
-
-    db.posts = db.posts.filter((post) => {
-      if (post.author !== user.id) return true;
-
-      db.comments = db.comments.filter((comment) => comment.post !== post.id);
-
-      return false;
-    });
-
-    db.comments = db.comments.filter(({ author }) => author !== user.id);
-
-    return user;
+    return prisma.mutation.deleteUser({ where: { id } }, info);
   },
   deletePost: (_parent, args, { db, pubsub }) => {
     const postIndex = db.posts.findIndex(({ id }) => id === args.id);
