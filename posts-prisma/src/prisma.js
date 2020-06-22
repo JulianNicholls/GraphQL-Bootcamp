33addd9328c2c2da87b7693a8c92e6336078eae7
Julian Nicholls <juliannicholls29@gmail.com>
2020-06-22 20:40:25 +0100

    Add secret to prisma

--- posts-prisma/src/prisma.js
+++ posts-prisma/src/prisma.js
@@ -3,6 +3,7 @@ import { Prisma } from 'prisma-binding';
 const prisma = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
   endpoint: 'http://localhost:4466',
+  secret: 'ghTYI478SDFGH67809GHJKtytiu',
 });
 
 export default prisma;
