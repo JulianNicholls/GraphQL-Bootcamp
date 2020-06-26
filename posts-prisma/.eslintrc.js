794a1b6a1928f857a659bf746d6282b87dc7274e
Julian Nicholls <juliannicholls29@gmail.com>
2020-06-26 15:42:17 +0100

    Set up eslint for jest

--- posts-prisma/.eslintrc.js
+++ posts-prisma/.eslintrc.js
@@ -1,18 +1,25 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2020": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 11,
-        "sourceType": "module"
-    },
-    "rules": {
-        "no-console": "off",
-        "linebreak-style": ["error", "unix"],
-        quotes: ["error", "single"],
-        semi: ["error", "always"],
-    }
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+    'jest/globals': true,
+  },
+  plugins: ['jest'],
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+  },
 };
