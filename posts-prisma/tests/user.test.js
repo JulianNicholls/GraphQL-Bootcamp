8b8ad0dd28497ce297c725e75e5538a30aaabe9c
Julian Nicholls <juliannicholls29@gmail.com>
2020-06-26 14:45:04 +0100

    Create basic tests

---
+++ posts-prisma/tests/user.test.js
@@ -0,0 +1,34 @@
+import { getFirstName, isValidPassword } from '../src/utils/user';
+
+describe('User', () => {
+  test('getFirstName should return first name', () => {
+    const first = getFirstName('Julian Nicholls');
+
+    expect(first).toBe('Julian');
+  });
+
+  test('getFirstName should be OK with just a first name', () => {
+    const first = getFirstName('Julian');
+
+    expect(first).toBe('Julian');
+  });
+
+  test('should reject short password', () => {
+    expect(isValidPassword('1234567')).toBe(false);
+  });
+
+  test('Should reject password with password in it', () => {
+    expect(isValidPassword('apassword')).toBe(false);
+    expect(isValidPassword('passworda')).toBe(false);
+    expect(isValidPassword('apassworda')).toBe(false);
+  });
+
+  test('Should accept valid passwords', () => {
+    expect(isValidPassword('12345678')).toBe(true);
+    expect(isValidPassword('123456789')).toBe(true);
+
+    expect(isValidPassword('apasswor')).toBe(true);
+    expect(isValidPassword('assworda')).toBe(true);
+    expect(isValidPassword('pass@word')).toBe(true);
+  });
+});
