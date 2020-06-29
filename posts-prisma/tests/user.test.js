0de61caa82411f2afc7be5b5341e7da410347905
Julian Nicholls <juliannicholls29@gmail.com>
2020-06-29 09:28:28 +0100

    Test signup and login with bad credentials

--- posts-prisma/tests/user.test.js
+++ posts-prisma/tests/user.test.js
@@ -40,7 +40,7 @@ beforeAll(async () => {
 });
 
 describe('User', () => {
-  test('should create a new user', async () => {
+  test('should create a new user with good credentials', async () => {
     const createUser = gql`
       mutation {
         createUser(
@@ -50,11 +50,6 @@ describe('User', () => {
             password: "password"
           }
         ) {
-          user {
-            id
-            name
-            email
-          }
           token
         }
       }
@@ -67,6 +62,24 @@ describe('User', () => {
     });
 
     expect(exists).toBe(true);
+  });
+
+  test('should not create a new user with bad credentials', async () => {
+    const createUserBad = gql`
+      mutation {
+        createUser(
+          data: {
+            name: "Julian"
+            email: "julian_n@example.com"
+            password: "pass" # Too short
+          }
+        ) {
+          token
+        }
+      }
+    `;
+
+    await expect(client.mutate({ mutation: createUserBad })).rejects.toThrow();
   });
 
   test('should expose only public author items', async () => {
@@ -84,6 +97,36 @@ describe('User', () => {
 
     expect(response.data.users).toHaveLength(2);
     expect(response.data.users[0].email).toBeNull();
+  });
+
+  test('should log in with correct credentials', async () => {
+    const login = gql`
+      mutation {
+        login(email: "jennifer@example.com", password: "Red101%#") {
+          user {
+            name
+          }
+          token
+        }
+      }
+    `;
+
+    const response = await client.mutate({ mutation: login });
+
+    expect(response.data.login.user.name).toBe('Jennifer');
+    expect(response.data.login.token).toHaveLength(177);
+  });
+
+  test('should fail to log in with bad credentials', async () => {
+    const loginBad = gql`
+      mutation {
+        login(email: "jennifer@example.com", password: "notpassword") {
+          token
+        }
+      }
+    `;
+
+    await expect(client.mutate({ mutation: loginBad })).rejects.toThrow();
   });
 });
 
